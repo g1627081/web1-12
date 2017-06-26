871202c63933e760037aca16f35a9fbdb03932ec
g1627081 <g1627081@st.t-kougei.ac.jp>
2017-06-26 18:00:24 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,10 +1,3 @@
-/*document.getElementById('form').onsubmit(){
-   window.alert('押したよ！');
-};
-document.getElementById('form').onsubmit = function(){
-   window.alert(document.getElementById('form').word.value);
-};
-*/
 for(var i= 1; i<101;i++)
 {
    var li = document.createElement('li');
@@ -22,3 +15,6 @@ for(var i= 1; i<101;i++)
    }
    document.getElementById('list').appendChild(li);
 }
+document.getElementById('form').onsubmit = function(){
+   window.alert(document.getElementById('form').word.value);
+};
