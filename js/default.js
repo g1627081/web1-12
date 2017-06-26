82f5b6b65c01941a1f540df5619efcec01b48fe3
g1627081 <g1627081@st.t-kougei.ac.jp>
2017-06-26 17:11:32 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,5 +2,5 @@ document.getElementById('form').onsubmit(){
    window.alert('押したよ！');
 };
 document.getElementById('form').onsubmit = function(){
-   window.alert(document.getElementById('form').word.value);
+   window.alert(document.getElementById('form').num.value);
 };
