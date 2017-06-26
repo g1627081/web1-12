fa2a98c623a9fff4c318830a0c90e309e6646f1c
g1627081 <g1627081@st.t-kougei.ac.jp>
2017-06-26 17:34:02 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,13 +1,24 @@
-document.getElementById('form').onsubmit(){
+/*document.getElementById('form').onsubmit(){
    window.alert('押したよ！');
 };
 document.getElementById('form').onsubmit = function(){
    window.alert(document.getElementById('form').num.value);
 };
-
-while(num)
+*/
+for(var i= 1; i<100;i++)
 {
    var li = document.createElement('li');
+   var con=Math.floor
    li.textContent=i;
+   
+   if(i%15=0){
+    li.textContent='Fizz';
+   }
+   else if(i%5=0){
+    li.textContent='Buzz';
+   }
+   else if(i%3=0){
+    li.textContent='FizzBuzz';
+   }
    document.getElementById('while').appendChild(li);
 }
