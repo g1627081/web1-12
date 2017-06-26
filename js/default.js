510fc44ce6e63ccfaff90cd830e8e091b0d3ccf6
g1627081 <g1627081@st.t-kougei.ac.jp>
2017-06-26 17:25:14 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -4,3 +4,10 @@ document.getElementById('form').onsubmit(){
 document.getElementById('form').onsubmit = function(){
    window.alert(document.getElementById('form').num.value);
 };
+
+while(num)
+{
+   var li = document.createElement('li');
+   li.textContent=i;
+   document.getElementById('while').appendChild(li);
+}
