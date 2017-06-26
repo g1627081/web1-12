e7399205200de66667f438794dc788508e8501f3
g1627081 <g1627081@st.t-kougei.ac.jp>
2017-06-26 17:39:24 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -8,8 +8,6 @@ document.getElementById('form').onsubmit = function(){
 for(var i= 1; i<100;i++)
 {
    var li = document.createElement('li');
-   li.textContent=i;
-   
    if(i%15=0){
     li.textContent='FizzBuzz';
    }
@@ -18,6 +16,9 @@ for(var i= 1; i<100;i++)
    }
    else if(i%3=0){
     li.textContent='Buzz';
+   }
+   else{
+   li.textContent=i;
    }
    document.getElementById('while').appendChild(li);
 }
