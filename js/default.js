56eade839c4fff16c46d8ee460e407e0bb324d0d
g1627081 <g1627081@st.t-kougei.ac.jp>
2017-06-26 17:53:23 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -5,7 +5,7 @@ document.getElementById('form').onsubmit = function(){
    window.alert(document.getElementById('form').word.value);
 };
 */
-for(var i= 1; i<100;i++)
+for(var i= 1; i<101;i++)
 {
    var li = document.createElement('li');
    if(i%15==0){
